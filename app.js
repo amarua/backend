06b8c8da2d2e9fc31580ed0bd00f0a519a403909
amarua <amarsarkar612@gmail.com>
2021-05-29 01:59:41 +0530

    backend initialized

---
+++ app.js
@@ -0,0 +1,81 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var app = express();
+//my code
+var test =require('./routes/test');
+const mysql=require('mysql');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+
+//Database:
+const db=mysql.createConnection({
+  host:'localhost',
+  port:3306,
+  user:'newuser',
+  database:'testing',
+  password:'password'
+})
+db.connect((err)=>{
+  if(err){
+    console.log(err);
+    throw err;
+    return false;
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+//This is my Code :
+app.use('/test', test);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ public/javascripts/passgen.js
@@ -0,0 +1,3 @@
+const bcrypt = require('bcrypt');
+let pswrd =bcrypt.hashSync('1234',10);
+console.log(pswrd);
